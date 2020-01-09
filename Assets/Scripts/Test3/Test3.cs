8932ee53c1330ccd3e9d7925c3efa1417268e1bd
lilei <279598616@qq.com>
2020-01-09 22:57:15 +0800

    添加Test3示例，用于展示自定义Loader的特性。

---
+++ Assets/Scripts/Test3/Test3.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class Test3 : MonoBehaviour
+{
+    LuaEnv luaEnv = new LuaEnv();
+    // Start is called before the first frame update
+    void Start()
+    {
+        luaEnv = new LuaEnv();
+        luaEnv.AddLoader(loader);
+        luaEnv.DoString("require 'Test3/Test3Main'");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    byte[] loader(ref string filename)
+    {
+        Debug.Log("filename:" + filename);
+        if (filename == "Test3")
+        {
+            string script = "return {test3 = 'test3 Hello World!'}";
+            return System.Text.Encoding.UTF8.GetBytes(script);
+        }
+        return null;
+    }
+}
