4071cc8a662b9a32521886bfda59d8cd6bc0ac96
lilei <279598616@qq.com>
2020-01-09 22:33:11 +0800

    初始工程上传

---
+++ Assets/Scripts/Test1/Test1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class Test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuaEnv luaenv = new LuaEnv();
+        luaenv.DoString("print('Hello World By print!')");
+        luaenv.DoString("CS.UnityEngine.Debug.Log('Hello World By Debug.Log')");
+        luaenv.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Test2/Test2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+
+public class Test2 : MonoBehaviour
+{
+    LuaEnv luaEnv = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        luaEnv = new LuaEnv();
+        luaEnv.DoString("require 'Test2/main'");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (luaEnv != null)
+        {
+            luaEnv.Tick();
+        }
+    }
+}
