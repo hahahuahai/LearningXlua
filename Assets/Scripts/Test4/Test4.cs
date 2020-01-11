9515e214d212e18396172afdc539ad54d12345e1
lilei <279598616@qq.com>
2020-01-11 09:16:43 +0800

    添加C#访问Lua示例。

---
+++ Assets/Scripts/Test4/Test4.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class LuaClass
+{
+    public int f1;
+    public int f2;
+}
+
+[CSharpCallLua]
+public interface LuaInterface
+{
+    int f1 { get; set; }
+    int f2 { get; set; }
+
+    int add(int a, int b);
+}
+
+public class Test4 : MonoBehaviour
+{
+    LuaEnv luaEnv = null;
+    [CSharpCallLua]
+    public delegate int AddDelegate(int a, int b);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        luaEnv = new LuaEnv();
+        luaEnv.DoString("require 'Test4/Test4Lua'");
+
+        Debug.Log("lua a:" + luaEnv.Global.Get<int>("a"));
+        Debug.Log("lua b:" + luaEnv.Global.Get<string>("b"));
+        Debug.Log("lua c:" + luaEnv.Global.Get<bool>("c"));
+
+        Debug.Log("lua t1.f1 by class:" + luaEnv.Global.Get<LuaClass>("t1").f1); //映射到普通class或struct,值拷贝
+        Debug.Log("lua t1.f2 by class:" + luaEnv.Global.Get<LuaClass>("t1").f2); //映射到普通class或struct,值拷贝
+
+        Debug.Log("lua t2.f1 by interface:" + luaEnv.Global.Get<LuaInterface>("t2").f1); //映射到一个interface,引用拷贝
+        Debug.Log("lua t2.f2 by interface:" + luaEnv.Global.Get<LuaInterface>("t2").f2); //映射到一个interface,引用拷贝
+
+        Debug.Log("lua t2.add by interface:" + luaEnv.Global.Get<LuaInterface>("t2").add(1, 2));//映射到一个interface,引用拷贝
+
+        Debug.Log("lua t2.f1 by Dictionary:" + luaEnv.Global.Get<Dictionary<string, float>>("t2")["f1"]);  //更轻量级的by value方式：映射到Dictionary<>
+        Debug.Log("lua t2.f2 by Dictionary:" + luaEnv.Global.Get<Dictionary<string, float>>("t2")["f2"]);  //更轻量级的by value方式：映射到Dictionary<>
+        Debug.Log("lua t2 Count by Dictionary:" + luaEnv.Global.Get<Dictionary<string, float>>("t2").Count);  //更轻量级的by value方式：映射到Dictionary<>
+
+        Debug.Log("lua t3 Count by List:" + luaEnv.Global.Get<List<double>>("t3").Count);  //更轻量级的by value方式：List<>
+
+        Debug.Log("lua t2.f1 by LuaTable:" + luaEnv.Global.Get<LuaTable>("t2").Get<int>("f1")); //另外一种by ref方式：映射到LuaTable类
+        Debug.Log("lua t2.f2 by LuaTable:" + luaEnv.Global.Get<LuaTable>("t2").Get<int>("f2")); //另外一种by ref方式：映射到LuaTable类
+
+        Debug.Log("lua addLua by delegate:" + luaEnv.Global.Get<AddDelegate>("addLua")(10,20)); //访问一个全局的function,映射到delegate
+
+        Debug.Log("lua addLua by LuaFunction:" + luaEnv.Global.Get<LuaFunction>("addLua").Call(12,22)[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # LearningXlua
 
-这个项目主要用于自学xlua，里面有自己针对每个xlua的特性对应的示例代码（assets/Scenes目录下），这些示例代码是在官方示例代码基础上进行编写的，有的类似，有的有所扩充。
+这个项目主要用于自学xlua，基于2020/01/09Xlua的版本。里面有自己针对每个xlua的特性对应的示例代码（assets/Scenes目录下），这些示例代码是在官方示例代码基础上进行编写的，有的类似，有的有所扩充。
 
 ## 示例代码说明
+
+示例代码是根据XLua官方文档进行编写，可以结合[文档]([https://github.com/Tencent/xLua/blob/master/Assets/XLua/Doc/XLua%E6%95%99%E7%A8%8B.md](https://github.com/Tencent/xLua/blob/master/Assets/XLua/Doc/XLua教程.md))来看。
 
 ### 1.Lua文件加载
 
@@ -14,7 +16,11 @@
 
 ### 2.C#访问Lua
 
+（1）获取一个全局基本数据类型：Test4.unity
 
+（2）访问一个全局的table：Test4.unity
+
+（3）访问一个全局的function：Test4.unity
 
 
 
